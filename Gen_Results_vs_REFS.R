685ae4c525ec6ebc4af01761bdda1e19481765d4
Bradley Butcher <bradleydpbutcher@gmail.com>
2019-02-04 12:24:14 +0000

    added ensemble metric support

--- Gen_Results_vs_REFS.R
+++ Gen_Results_vs_REFS.R
@@ -42,7 +42,8 @@ get.results.wvstructs <- function(true, learnt, label) {
     dag.f1 <- 2 * (dag.precision * dag.recall) / (dag.precision + dag.recall)
     
     c(skeleton.precision, skeleton.recall, skeleton.f1,
-      v.results$precision, v.results$recall, v.f1, dag.precision, dag.recall, dag.f1)
+      v.results$precision, v.results$recall, v.f1, 
+      dag.precision, dag.recall, dag.f1)
     
   }, error = function(e){
     return(rep('ERROR', 12))
@@ -53,8 +54,38 @@ get.results.wvstructs <- function(true, learnt, label) {
   return(res)
 }
 
+get.ensemble.results <- function(true, learnt.models, label) {
+  results <- c()
+  n <- length(learnt.models)
+  for(learnt.model in learnt.models) {
+    single.res <- get.results.wvstructs(true, model2network(learnt.model), label)
+    results <- c(results, unlist(single.res))
+  }
+  summarized.results <- c('mean.skeleton.precision' = mean(results[(1:n * 9) - 8]),
+              'std.skeleton.precision' = sd(results[(1:n * 9) - 8]),
+              'mean.skeleton.recall' = mean(results[(1:n * 9) - 7]),
+              'std.skeleton.recall' = sd(results[(1:n * 9) - 7]),
+              'mean.skeleton.f1' = mean(results[(1:n * 9) - 6]),
+              'std.skeleton.f1' = sd(results[(1:n * 9) - 6]),
+              'mean.v.precision' = mean(results[(1:n * 9) - 5]),
+              'std.v.precision' = sd(results[(1:n * 9) - 5]),
+              'mean.v.recall' = mean(results[(1:n * 9) - 4]),
+              'std.v.recall' = sd(results[(1:n * 9) - 4]),
+              'mean.v.f1' = mean(results[(1:n * 9) - 3]),
+              'std.v.f1' = sd(results[(1:n * 9) - 3]),
+              'mean.dag.precision' = mean(results[(1:n * 9) - 2]),
+              'std.dag.precision' = sd(results[(1:n * 9) - 2]),
+              'mean.dag.recall' = mean(results[(1:n * 9) - 1]),
+              'std.dag.recall' = sd(results[(1:n * 9) - 1]),
+              'mean.dag.f1' = mean(results[(1:n * 9)]),
+              'std.dag.f1' = sd(results[(1:n * 9)])
+              )
+  names(summarized.results) <- paste0(names(summarized.results), ' ', label)
+  return(summarized.results)
+}
+
 gen.results <- function(results.filename, subsample.directory, modelstring.directory, 
-                        algorithm, algorithm.name, algorithm.parameters, result.savedir) {
+                        algorithm, algorithm.name, algorithm.parameters, result.savedir, ensemble = FALSE) {
   #Load Results
   results.df <- read.csv(results.filename)
   
@@ -75,7 +106,7 @@ gen.results <- function(results.filename, subsample.directory, modelstring.direc
     row <- results.df[i, ]
     #Load True Structure
     true.string <- row$truth_modelstring
-    true.model <- model2network(as.character(true.string))
+    true.model <- bnlearn::model2network(as.character(true.string))
 
     #Get subsample data
     datafile <- row$input_datafile
@@ -92,7 +123,7 @@ gen.results <- function(results.filename, subsample.directory, modelstring.direc
     modelstring.file.start <- paste0(modelstring.directory, '/REFS_', row$input_structuretype,
                                      '_node', row$input_nodes, '_sample', row$input_samples, '_*')
     modelstring.file <- Sys.glob(modelstring.file.start)[1]
-    refs.learnt <- model2network(strsplit(read_file(modelstring.file), '[\\\\]|[^[:print:]]')[[1]][1])
+    refs.learnt <- bnlearn::model2network(strsplit(read_file(modelstring.file), '[\\\\]|[^[:print:]]')[[1]][1])
     
     #Calculate REFS results
     refs.results <- get.results.wvstructs(true.model, refs.learnt, 'REFS')
@@ -100,12 +131,23 @@ gen.results <- function(results.filename, subsample.directory, modelstring.direc
     #Get Algorithm Learnt Model
     algorithm.learnt <- do.call(algorithm, append(list(data = data), algorithm.parameters))
     
+    algorithm.results <- NULL
+    ensemble.results <- NULL
     #Get algorithm results
-    algorithm.results <- get.results.wvstructs(true.model, algorithm.learnt, algorithm.name)
+    if(!ensemble) {
+      algorithm.results <- get.results.wvstructs(true.model, algorithm.learnt, algorithm.name)
+    } else {
+      algorithm.results <- get.results.wvstructs(true.model, algorithm.learnt$consensus.dag, algorithm.name)
+      ensemble.results <- get.ensemble.results(true.model, algorithm.learnt$ensemble.models, algorithm.name)
+    }
     
     #Store results in row
-    results.row <- c('Structure Type' = as.character(names(struct.conv)[which(struct.conv %in% row$input_structure)]), 'Number of Samples' = row$input_samples,
+    results.row <- c('Structure Type' = as.character(row$input_structuretype), 'Number of Samples' = row$input_samples,
                      'Number of Nodes' = row$input_nodes, 'N.Params' = n.params, refs.results, algorithm.results)
+    
+    if(ensemble) {
+      results.row <- c(results.row, ensemble.results)
+    }
     
     results <- rbind(results, results.row)
     write.csv(results, result.savedir, row.names = FALSE)
@@ -113,10 +155,14 @@ gen.results <- function(results.filename, subsample.directory, modelstring.direc
   return(results)
 }
 
-results <- gen.results('~/Desktop/REFS_Results/notes_20181121_01.csv',
-                       '~/Desktop/REFS_Results/data/',
-                       '~/Desktop/REFS_Results/',
+setwd('~/Desktop/Surgo/')
+source('~/Desktop/Surgo/Experimental_Structure_Learning/Cluster_PCTABU.R')
+source('~/Desktop/Surgo/Experimental_Structure_Learning/pair_experiment.R')
+results <- gen.results('Benchmarking/REFS_Results/notes_20181121_01.csv',
+                       'Benchmarking/REFS_Results/data/',
+                       'Benchmarking/REFS_Results/',
                        dag.threshold.ensemble,
                        'Clustered.Fragments.PCTABU',
-                       list(n = 20),
-                       'Clustered_Fragments_PCTABU.csv')
+                       list(n = 2),
+                       'Clustered_Fragments_PCTABU_Ens.csv',
+                       ensemble = TRUE)
