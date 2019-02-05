0cafa51e20cf1cf3308dc4d0b32f99c4b78b283b
Bradley <bradleydpbutcher@gmail.com>
2019-02-05 10:58:06 +0000

    Added parallelization to the benchmarking

--- Gen_Results_vs_REFS.R
+++ Gen_Results_vs_REFS.R
@@ -1,4 +1,4 @@
-library(readr)
+library(doParallel)
 
 v.structs.match <- function(comparison.structure, true.structure) {
   #True V structure in a dataframe
@@ -21,15 +21,14 @@ v.structs.match <- function(comparison.structure, true.structure) {
 }
 
 get.results.wvstructs <- function(true, learnt, label) {
-  res <- tryCatch({
     labels <- c(paste0(label, ' Precision'), paste0(label, ' Recall'), paste0(label, ' F1'), 
                 paste0(label, ' V Precision'), paste0(label, ' V Recall'), paste0(label, ' V F1'),
                 paste0(label, ' DAG Precision'), paste0(label, ' DAG Recall'), paste0(label, ' DAG F1'))
     
     v.results <-  v.structs.match(learnt, true)
-    skeleton.results <- bnlearn::compare(bnlearn::skeleton(true),
-                                         bnlearn::skeleton(learnt))
-    dag.results <- bnlearn::compare(true, learnt)
+    skeleton.results <- compare(skeleton(true),
+                                         skeleton(learnt))
+    dag.results <- compare(true, learnt)
     
     skeleton.precision <- skeleton.results$tp / (skeleton.results$tp + skeleton.results$fp)
     skeleton.recall <- skeleton.results$tp / (skeleton.results$tp + skeleton.results$fn)
@@ -41,13 +40,10 @@ get.results.wvstructs <- function(true, learnt, label) {
     dag.recall <- dag.results$tp / (dag.results$tp + dag.results$fn)
     dag.f1 <- 2 * (dag.precision * dag.recall) / (dag.precision + dag.recall)
     
-    c(skeleton.precision, skeleton.recall, skeleton.f1,
+    res <- c(skeleton.precision, skeleton.recall, skeleton.f1,
       v.results$precision, v.results$recall, v.f1, 
       dag.precision, dag.recall, dag.f1)
     
-  }, error = function(e){
-    return(rep('ERROR', 12))
-  })
   
   names(res) <- labels
   
@@ -85,29 +81,42 @@ get.ensemble.results <- function(true, learnt.models, label) {
 }
 
 gen.results <- function(results.filename, subsample.directory, modelstring.directory, 
-                        algorithm, algorithm.name, algorithm.parameters, result.savedir, ensemble = FALSE) {
+                        algorithm, algorithm.name, algorithm.parameters, algorithm.source,
+                        result.savedir, n.workers = 5, ensemble = FALSE) {
   #Load Results
   results.df <- read.csv(results.filename)
   
   struct.conv <- list('forest fire' = 'ForestFire', 'ic-dag' = 'icDAG', 'preferential attachment' = 'PrefAttach')
   
   results <- NULL
-  
-  start.idx <- 1
+  experiments.todo <- NULL
   
   if(file_test("-f", result.savedir)) {
     results <- read.csv(result.savedir)
-    start.idx <- nrow(results) + 1
-    print(paste0('Resuming from experiment #', start.idx))
+    performed.experiments <- results$Experiment
+    experiments.todo <- setdiff(1:nrow(results.df), performed.experiments)
+    print('Resuming Experiment')
+  } else {
+    experiments.todo <- 1:nrow(results.df)
   }
   
+  cluster <<- makeCluster(n.workers, outfile='')
+  registerDoParallel(cluster)
+  on.exit(print('Stopping Cluster...'))
+  on.exit(stopCluster(cluster))
+  
   #Iterate over rows
-  for(i in start.idx:nrow(results.df)) {
+  final.results <- foreach(i=experiments.todo,
+                           .export=c('get.results.wvstructs', 'get.ensemble.results', 'v.structs.match'),
+                           .packages=c('bnlearn', 'readr'), 
+                           .combine=rbind) %dopar% {
+    source(algorithm.source, local = TRUE)
+    print(paste0('Starting experiment ', i))
     row <- results.df[i, ]
     #Load True Structure
     true.string <- row$truth_modelstring
-    true.model <- bnlearn::model2network(as.character(true.string))
-
+    true.model <- model2network(as.character(true.string))
+    
     #Get subsample data
     datafile <- row$input_datafile
     datafile <- tail(strsplit(as.character(datafile), split="/", fixed=TRUE)[[1]], 1)
@@ -117,13 +126,14 @@ gen.results <- function(results.filename, subsample.directory, modelstring.direc
     data[] <- lapply(data, as.factor)
     
     #Get Number of Parameters
-    n.params <- bnlearn::nparams(true.model, data)
+    n.params <- nparams(true.model, data)
     
     #Get Refs learnt model
     modelstring.file.start <- paste0(modelstring.directory, '/REFS_', row$input_structuretype,
                                      '_node', row$input_nodes, '_sample', row$input_samples, '_*')
+    
     modelstring.file <- Sys.glob(modelstring.file.start)[1]
-    refs.learnt <- bnlearn::model2network(strsplit(read_file(modelstring.file), '[\\\\]|[^[:print:]]')[[1]][1])
+    refs.learnt <- model2network(strsplit(read_file(modelstring.file), '[\\\\]|[^[:print:]]')[[1]][1])
     
     #Calculate REFS results
     refs.results <- get.results.wvstructs(true.model, refs.learnt, 'REFS')
@@ -133,7 +143,7 @@ gen.results <- function(results.filename, subsample.directory, modelstring.direc
     
     algorithm.results <- NULL
     ensemble.results <- NULL
-    #Get algorithm results
+    #Get algorithm result
     if(!ensemble) {
       algorithm.results <- get.results.wvstructs(true.model, algorithm.learnt, algorithm.name)
     } else {
@@ -142,27 +152,51 @@ gen.results <- function(results.filename, subsample.directory, modelstring.direc
     }
     
     #Store results in row
-    results.row <- c('Structure Type' = as.character(row$input_structuretype), 'Number of Samples' = row$input_samples,
+    results.row <- c('Experiment' = i, 'Structure Type' = as.character(row$input_structuretype), 'Number of Samples' = row$input_samples,
                      'Number of Nodes' = row$input_nodes, 'N.Params' = n.params, refs.results, algorithm.results)
     
     if(ensemble) {
       results.row <- c(results.row, ensemble.results)
     }
-    
-    results <- rbind(results, results.row)
-    write.csv(results, result.savedir, row.names = FALSE)
+
+    if(!file_test("-f", result.savedir)) {
+      print('Saving New CSV')
+      cat.conn <- file(result.savedir)
+      cat(names(results.row), file = cat.conn, sep = ",")
+      close(cat.conn)
+      
+      cat.conn <- file(result.savedir, open = "a")
+      cat('\n', file = cat.conn)
+      cat(unname(results.row), file = cat.conn, sep = ",")
+      close(cat.conn)
+      
+    } else {
+      print('Appending to CSV')
+      cat.conn <- file(result.savedir, open = "a")
+      cat('\n', file = cat.conn)
+      cat(unname(results.row), file = cat.conn, sep = ",")
+      close(cat.conn)
+    }
+    results.row
   }
+  write.csv(final.results, result.savedir, row.names = FALSE)
+  stopCluster(cluster)
+  
   return(results)
 }
 
-setwd('~/Desktop/Surgo/')
-source('~/Desktop/Surgo/Experimental_Structure_Learning/Cluster_PCTABU.R')
-source('~/Desktop/Surgo/Experimental_Structure_Learning/pair_experiment.R')
-results <- gen.results('Benchmarking/REFS_Results/notes_20181121_01.csv',
-                       'Benchmarking/REFS_Results/data/',
-                       'Benchmarking/REFS_Results/',
-                       dag.threshold.ensemble,
+####################### EXAMPLE USAGE ############################
+
+setwd('/home/surgo')
+source('SL_Methods/Cluster_PCTABU.R')
+
+results <- gen.results('SL_Benchmarking/REFS_Results/notes_20181121_01.csv',
+                       'SL_Benchmarking/REFS_Results/data/',
+                       'SL_Benchmarking/REFS_Results/',
+                       cluster.pctabu.ensemble,
                        'Clustered.Fragments.PCTABU',
-                       list(n = 2),
-                       'Clustered_Fragments_PCTABU_Ens.csv',
+                       list(n = 20),
+                       'SL_Methods/Cluster_PCTABU.R',
+                       'Clustered_Fragments_PCTABU_ENS.csv',
+                       n.workers = 10,
                        ensemble = TRUE)
